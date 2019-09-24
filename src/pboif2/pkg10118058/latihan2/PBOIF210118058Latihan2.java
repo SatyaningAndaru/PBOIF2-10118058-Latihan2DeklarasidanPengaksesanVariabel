acb7c1838b94b8e586dea73404cb5670932803c9
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-09-24 17:49:55 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118058/latihan2/PBOIF210118058Latihan2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118058.latihan2;
+
+/**
+ *
+ * @author user
+ * Nama         :   Satyaning Andaru Bawalaksana
+ * Nim          :   10118058
+ * Kelas        :   PBOIF2
+ * Deskripsi    :   Program ini berisi program menampilkan
+ * Jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210118058Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int  nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter ='D';
+        
+        //menampilkan hasil
+        System.out.println();
+        System.out.println("isi variabel nilai = " + nilaiInt);
+        System.out.println("isi variabel PHI = " + PHI);
+        System.out.println("isi variabel logika = " + nilaiLogika);
+        System.out.println("isi variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
